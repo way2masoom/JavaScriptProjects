a9bde4cdd267fe555721f9f17698efa599cfaa26
MD ALAM <alkaserphone@MacBook-Air.local>
2026-04-11 08:49:50 +0530

    Implement dynamic keyboard button generation in JavaScript

---
+++ HangmanGame/script.js
@@ -0,0 +1,8 @@
+const keyboardDiv = document.querySelector(".keyboard");
+
+// Creating keyboard button
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+}
