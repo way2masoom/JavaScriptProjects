107d07fc7908d9c4c85e2d6006ec8f4f3e898f03
MD ALAM <alkaserphone@MacBook-Air.local>
2026-04-11 08:55:37 +0530

    Creted new folder for script

--- HangmanGame/scripts/script.js
+++ HangmanGame/scripts/script.js
@@ -1,8 +1,10 @@
 const keyboardDiv = document.querySelector(".keyboard");
 
 // Creating keyboard button
-for (let i = 97; i <= 122; i++) {
+for (let i = 97; i <= 122; i++) { // 97 → 122 → (a → z)
     const button = document.createElement("button");
+    // Converts ASCII → character
     button.innerText = String.fromCharCode(i);
     keyboardDiv.appendChild(button);
 }
+
